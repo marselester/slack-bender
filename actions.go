94b92f8e6650bc7ccd53b7b5f916c15ac29cd896
marselester <marselester@ya.ru>
2015-06-17 14:36:23 +0700

    Add dummy reaction on mentioning a bot in a channel

--- actions.go
+++ actions.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func listenAndReact(rtm *RtmStart, inChan chan *InboundEvent, errChan chan *ErrorEvent, outChan chan *OutboundEvent) {
@@ -9,9 +10,27 @@ func listenAndReact(rtm *RtmStart, inChan chan *InboundEvent, errChan chan *Erro
 	for {
 		select {
 		case event := <-inChan:
+			go react(rtm, event, outChan)
 			fmt.Println("Message:", event.User, event.Channel, event.Text)
 		case event := <-errChan:
 			fmt.Println("Error:", event.Type, event.Error)
 		}
 	}
+}
+
+// Bot reacts on messages where it was mentioned, for example,
+// <@U04KYSKPB>: how are you doing?
+func react(rtm *RtmStart, msg *InboundEvent, outChan chan *OutboundEvent) {
+	botID := rtm.Self.ID
+	isBotMentioned := strings.Contains(msg.Text, botID)
+	if !isBotMentioned {
+		return
+	}
+
+	resp := &OutboundEvent{
+		Type:    "message",
+		Channel: msg.Channel,
+		Text:    "Wazzup",
+	}
+	outChan <- resp
 }
